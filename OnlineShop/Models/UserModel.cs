373062bbe800b7119ae8efff4025dab8d2e41c5c
Thai Toan <you@example.comtoanlethai1@gmail.com>
2021-04-14 23:15:03 +0700

    add new file

---
+++ OnlineShop/Models/UserModel.cs
@@ -0,0 +1,23 @@
+﻿using Models.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class UserModel
+    {
+        private OnlineShopDBContent context = null;
+
+        public UserModel()
+        {
+            context = new OnlineShopDBContent();
+        }
+        public bool Login(string UserName, string Password)
+        {
+            var res = context.Database.SqlQuery<bool>("");
+        }
+    }
+}
